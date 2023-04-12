1715069793c6e2b34bce6824b93f163e81dc75e2
shanti-nb <shanti.chouhan@noborderz.com>
2023-04-12 17:14:09 +0530

    Add deployment automation scripts

---
+++ deploy/work.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+### Configuration ###
+
+APP_DIR=/var/www/test-app-dep
+GIT_URL=https://github.com/shanti-nb/test-app-dep.git
+RESTART_ARGS=
+
+# Uncomment and modify the following if you installed Passenger from tarball
+#export PATH=/path-to-passenger/bin:$PATH
+
+
+### Automation steps ###
+
+set -x
+
+# Pull latest code
+if [[ -e test-app-dep/code ]]; then
+  cd test-app-dep/code
+  git pull
+else
+  git clone https://github.com/shanti-nb/test-app-dep.git test-app-dep/code
+  cd test-app-dep/code
+fi
+
+# Install dependencies
+npm install --production
+npm prune --production
+
+# Restart app
+passenger-config restart-app --ignore-app-not-running --ignore-passenger-not-running $RESTART_ARGS $APP_DIR/code
