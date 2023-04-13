184edb47e0a0054e16776092db794064b91f875f
shanti-nb <shanti.chouhan@noborderz.com>
2023-04-13 16:16:38 +0530

    script check

---
+++ deploy/work-prod.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+### Configuration ###
+
+APP_DIR=test-app-dep
+GIT_URL=https://github.com/shanti-nb/test-app-dep.git
+RESTART_ARGS=
+
+# Uncomment and modify the following if you installed Passenger from tarball
+#export PATH=/path-to-passenger/bin:$PATH
+
+
+### Automation steps ###
+
+set -x
+
+# Pull latest code
+if [[ -e $APP_DIR ]]; then
+  cd $APP_DIR
+  git pull
+else
+  git clone $GIT_URL
+  cd $APP_DIR
+fi
+
+# Install dependencies
+npm install --production
+npm prune --production
+
+# Restart app
+ssh -t ubuntu@54.251.10.54 "source ~/.nvm/nvm.sh && cd test-app-dep && git pull origin main && npm ci && pm2 start npm --name my_app_my -- start"
